361213bc7845926372b48dfc8729386537c8b943
Saurav Sharma <2003saurav@gmail.com>
2025-07-01 23:54:21 -0400

    updating warnings settings

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  eslint: {
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    domains: ['cdn.futwiz.com', 'ui-avatars.com', 'b.fssta.com', 'sortitoutsidospaces.b-cdn.net'],
+  },
+}
 
-export default nextConfig;
+module.exports = nextConfig
